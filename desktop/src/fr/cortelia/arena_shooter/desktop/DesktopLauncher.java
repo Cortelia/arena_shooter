1ae767dfdbc56cab25a788702f5c9fa252445927
Roikubo <loic.corbon@gmail.com>
2018-06-13 20:27:51 +0200

    v 0.0.1 Project initial commit
    Creating project environment using libGDX setup app
        target release supports : Desktop, Html
        Extensions added : Box2d, Box2dlights
    Adding basic packages to the core project

---
+++ desktop/src/fr/cortelia/arena_shooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.cortelia.arena_shooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.cortelia.arena_shooter.ArenaShooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArenaShooter(), config);
+	}
+}
